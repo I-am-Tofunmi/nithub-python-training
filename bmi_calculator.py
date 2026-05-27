a254967b7efcd932efa657b920048c47d2aebe38
I-am-Tofunmi <frostycalmen@gmail.com>
2026-05-27 20:19:03 +0100

    save changes

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,9 +2,22 @@ weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in m: "))
 
 def bmi_calculator(weight, height):
+    if height <= 0 or weight <= 0:
+        print("Height and weight must be greater than zero.")
+        return None
+    
     height_sq = height ** 2
     bmi_value = weight / height_sq
     return bmi_value
 
 bmi_value = bmi_calculator(weight, height)
-print("Your BMI is: {:.2f}".format(bmi_value))
+if bmi_value is not None:
+    print("Your BMI is: {:.2f}".format(bmi_value))
+    if bmi_value < 18.5:
+        print("You are underweight.")
+    elif 18.5 <= bmi_value < 25:
+        print("You have a normal weight.")
+    elif 25 <= bmi_value < 30:
+        print("You are overweight.")
+    elif bmi_value >= 30:
+        print("You are obese.")
